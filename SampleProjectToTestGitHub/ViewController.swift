0ed3a4f66046e0a4f5189427b6077ec6924042db
mprashantt <tprashant4648010@gmail.com>
2019-06-19 12:59:33 +0530

    Initial Commit

---
+++ SampleProjectToTestGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProjectToTestGitHub
+//
+//  Created by Prashant Tripathi on 19/06/19.
+//  Copyright © 2019 Prashant Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
